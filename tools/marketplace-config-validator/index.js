4c3c5099a4b732505e5c94e890411f4b89de865f
tom <tom@ohhhh.me>
2023-03-31 16:18:24 -0300

    validate all config files in folder

--- tools/marketplace-config-validator/index.js
+++ tools/marketplace-config-validator/index.js
@@ -1,6 +1,40 @@
-const baseGoerliConfig = require('../../configs/marketplace/base-goerli.json')
 const { appItemOverviewSchema } = require('./schema.js')
 
-console.log('Checking base-goerli.json...')
-appItemOverviewSchema.array().parse(baseGoerliConfig)
-console.log('All good!')
+var fs = require('fs')
+var path = require('path')
+
+function readFiles(dirname, onFileContent, onError) {
+	fs.readdir(dirname, function (err, filenames) {
+		if (err) {
+			onError(err)
+			return
+		}
+		filenames.forEach(function (filename) {
+			fs.readFile(
+				path.resolve(dirname, filename),
+				'utf-8',
+				function (err, content) {
+					if (err) {
+						onError(err)
+						return
+					}
+					onFileContent(filename, content)
+				}
+			)
+		})
+	})
+}
+
+console.log()
+readFiles(
+	__dirname + '/../../configs/marketplace',
+	function (filename, content) {
+		console.log(`⏳ Checking ${filename}...`)
+		appItemOverviewSchema.array().parse(JSON.parse(content))
+		console.log('👍 All good!')
+		console.log()
+	},
+	function (err) {
+		throw err
+	}
+)
