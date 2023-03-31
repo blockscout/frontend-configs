d0a48190b221f3a1f11ae85de1bfb6e0ea20e5c9
tom <tom@ohhhh.me>
2023-03-31 14:01:17 -0300

    json validator script

---
+++ tools/marketplace-config-validator/index.js
@@ -0,0 +1,6 @@
+const baseGoerliConfig = require('../../configs/marketplace/base-goerli.json') ;
+const { appItemOverviewSchema } = require('./schema.js');
+
+console.log('Checking base-goerli.json...');
+appItemOverviewSchema.array().parse(baseGoerliConfig);
+console.log('All good!');
