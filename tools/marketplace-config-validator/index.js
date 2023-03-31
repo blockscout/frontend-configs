b9fed1ca1fe5f913bc581663801acb769ec29716
tom <tom@ohhhh.me>
2023-03-31 14:36:12 -0300

    add prettier

--- tools/marketplace-config-validator/index.js
+++ tools/marketplace-config-validator/index.js
@@ -1,6 +1,6 @@
-const baseGoerliConfig = require('../../configs/marketplace/base-goerli.json') ;
-const { appItemOverviewSchema } = require('./schema.js');
+const baseGoerliConfig = require('../../configs/marketplace/base-goerli.json')
+const { appItemOverviewSchema } = require('./schema.js')
 
-console.log('Checking base-goerli.json...');
-appItemOverviewSchema.array().parse(baseGoerliConfig);
-console.log('All good!');
+console.log('Checking base-goerli.json...')
+appItemOverviewSchema.array().parse(baseGoerliConfig)
+console.log('All good!')
--- tools/marketplace-config-validator/types.ts
+++ tools/marketplace-config-validator/types.ts
@@ -1,23 +1,34 @@
-export type MarketplaceCategoriesIds = 'all' | 'favorites' | 'defi' | 'exchanges' | 'finance' | 'games' | 'marketplaces' | 'nft' |
-'security' | 'social' | 'tools' | 'yieldFarming';
+export type MarketplaceCategoriesIds =
+	| 'all'
+	| 'favorites'
+	| 'defi'
+	| 'exchanges'
+	| 'finance'
+	| 'games'
+	| 'marketplaces'
+	| 'nft'
+	| 'security'
+	| 'social'
+	| 'tools'
+	| 'yieldFarming'
 
 export type MarketplaceCategory = { id: MarketplaceCategoriesIds; name: string }
 
 export type AppItemPreview = {
-  id: string;
-  external?: boolean;
-  title: string;
-  logo: string;
-  shortDescription: string;
-  categories: Array<MarketplaceCategoriesIds>;
-  url: string;
+	id: string
+	external?: boolean
+	title: string
+	logo: string
+	shortDescription: string
+	categories: Array<MarketplaceCategoriesIds>
+	url: string
 }
 
 export type AppItemOverview = AppItemPreview & {
-  author: string;
-  description: string;
-  site?: string;
-  twitter?: string;
-  telegram?: string;
-  github?: string;
+	author: string
+	description: string
+	site?: string
+	twitter?: string
+	telegram?: string
+	github?: string
 }
