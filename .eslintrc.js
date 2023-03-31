74e5c453c467d722155e96ef7251d1171341fe4f
tom <tom@ohhhh.me>
2023-03-31 14:19:50 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+      "plugin:jsonc/recommended-with-json",
+    ]
+  };
