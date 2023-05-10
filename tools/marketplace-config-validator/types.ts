54eeb691d4459d7150b12599b9e66fa96bf01469
tom <tom@ohhhh.me>
2023-05-10 11:57:22 -0300

    dark mode logo of marketplace app

--- tools/marketplace-config-validator/types.ts
+++ tools/marketplace-config-validator/types.ts
@@ -17,6 +17,7 @@ export type AppItemPreview = {
 	external?: boolean
 	title: string
 	logo: string
+	logoDarkMode?: string
 	shortDescription: string
 	categories: Array<MarketplaceCategoriesIds>
 	url: string
