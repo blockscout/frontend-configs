95cff6a4e6005891847f928cc097ec8d32ef6ea2
tom <tom@ohhhh.me>
2025-12-11 12:30:27 +0100

    hero banner config for japan

--- tools/json-converter/index.js
+++ tools/json-converter/index.js
@@ -1,17 +1,17 @@
-const fs = require('fs')
+import fs from 'fs'
 
 const jsonFile = process.argv[2]
 
 if (!jsonFile) {
-	console.error('Please provide a JSON file as an argument')
-	process.exit(1)
+  console.error('Please provide a JSON file as an argument')
+  process.exit(1)
 }
 
 const jsonData = fs.readFileSync(jsonFile, 'utf8')
 const jsonObject = JSON.parse(jsonData)
 
 const singleLineString = JSON.stringify(jsonObject)
-	.replace(/"/g, "'")
-	.replace(/,\s*/g, ',')
+  .replace(/"/g, "'")
+  .replace(/,\s*/g, ',')
 
 console.log(singleLineString)
