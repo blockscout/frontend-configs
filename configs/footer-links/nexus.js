d60660e226dbc64ca1bc0611a1e9897e89c6852c
tom <tom@ohhhh.me>
2026-05-07 11:09:11 +0200

    assets for nexus chain

---
+++ configs/footer-links/nexus.js
@@ -0,0 +1,38 @@
+import partners from './common/partners.js'
+export default [
+  {
+    title: 'Developers',
+    links: [
+      {
+        text: 'Docs',
+        url: 'https://docs.nexus.xyz',
+      },
+      {
+        text: 'GitHub',
+        url: 'https://github.com/nexus-xyz',
+      },
+    ],
+  },
+  {
+    title: 'About',
+    links: [
+      {
+        text: 'X',
+        url: 'https://x.com/NexusLabs',
+      },
+      {
+        text: 'Discord',
+        url: 'https://discord.com/invite/nexus-xyz',
+      },
+      {
+        text: 'Telegram',
+        url: 'https://t.me/nexus_zkvm',
+      },
+      {
+        text: 'YouTube',
+        url: 'https://www.youtube.com/@NexusLabsHQ',
+      },
+    ],
+  },
+  partners,
+]
